1055c83668bc09f63d4df8251cd49a0b84b1b8ff
João Pedro <joao.profissional.45@gmail.com>
2022-09-15 11:30:31 -0300

    Correção no main

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const path = require('path');
 
 const app = express();
 
-// Database
-const User = require('./src/database/schema/UserSchema');
-
 // Usos do servidor
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    return res.sendFile(path.join(__dirname, '/public/index.html'));
+    return res.render(path.join(__dirname, '/public/'));
 });
 
 app.listen(process.env.PORT, () => {
